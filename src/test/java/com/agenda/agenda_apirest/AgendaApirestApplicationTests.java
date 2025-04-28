118b155fb4fc8fdc911236faf1476d9891ead962
MaxLRxd <maximorojas002@gmail.com>
2025-04-27 22:53:40 -0300

    correccion de errores

--- src/test/java/com/agenda/agenda_apirest/AgendaApirestApplicationTests.java
+++ src/test/java/com/agenda/agenda_apirest/AgendaApirestApplicationTests.java
@@ -3,11 +3,13 @@ package com.agenda.agenda_apirest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class AgendaApirestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Si llega hasta acá, el contexto arrancó sin errores
+    }
 
 }
